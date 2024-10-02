4997a7a4df187ce51c412e52507ef031f7dd40f5
Pratham Srivastava <114439561+pratham534@users.noreply.github.com>
2024-10-02 12:45:41 +0530

    Time: 2 ms (60.91%), Space: 7.4 MB (61.55%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.cpp
+++ 1086-divisor-game/1086-divisor-game.cpp
@@ -1,23 +1,7 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        vector<vector<bool>> dp(2,vector<bool>(n+1));
-        for(int i=0;i<=n;i++){
-            dp[0][i]=false;
-            dp[1][i]=true;
-        }
-        dp[0][1]=false;
-        dp[1][1]=true;
-        // dp[0][2]=true;
-        // dp[1][2]=false;
-        for(int i=2;i<=n;i++){
-            for(int j=1;j<i;j++){
-                if(i%j == 0){
-                    dp[0][i] = dp[0][i] | dp[1][(i-j)];
-                    dp[1][i] = dp[1][i] & dp[0][(i-j)];
-                }
-            }
-        }
-        return dp[0][n];
+        if(n%2==0) return true;
+        return false;
     }
 };
