3e952e676f03698a301829f44b495033ad60496a
Pratham Srivastava <114439561+pratham534@users.noreply.github.com>
2024-10-05 11:44:23 +0530

    Time: 27 ms (22.96%), Space: 51 MB (23.19%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,12 +1,22 @@
 class Solution {
     private int[][] dp;
-    public int findLength(int i, int j, String text1, String text2){
-        if(i<=0 || j<=0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(text1.charAt(i-1)==text2.charAt(j-1)){
-            return dp[i][j] = (1 + findLength(i-1, j-1, text1, text2));
+    public int findLength(int n, int m, String text1, String text2){
+        // if(i<=0 || j<=0) return 0;
+        // if(dp[i][j]!=-1) return dp[i][j];
+        // if(text1.charAt(i-1)==text2.charAt(j-1)){
+        //     return dp[i][j] = (1 + findLength(i-1, j-1, text1, text2));
+        // }
+        // return dp[i][j] = Math.max(findLength(i,j-1,text1,text2),findLength(i-1,j,text1,text2));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
         }
-        return dp[i][j] = Math.max(findLength(i,j-1,text1,text2),findLength(i-1,j,text1,text2));
+        return dp[n][m];
     }
     public String printLCS(String text1, int n, String text2, int m){
         // for(int i=0;i<=n;i++){
@@ -37,6 +47,7 @@ class Solution {
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
                 dp[i][j]=-1;
+                if(i==0 || j==0) dp[i][j]=0;
             }
         }
         int ans = findLength(n,m,text1,text2);
