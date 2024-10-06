2a85a2c8ff10999f9a1570b89f0440e18f67ce2b
Pratham Srivastava <114439561+pratham534@users.noreply.github.com>
2024-10-06 18:12:30 +0530

    Time: 21 ms (84.51%), Space: 50.1 MB (71.79%) - LeetHub

---
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
@@ -0,0 +1,56 @@
+class Solution {
+    public String shortestCommonSupersequence(String str1, String str2) {
+        StringBuilder str = new StringBuilder();
+        int n = str1.length(), m = str2.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=m;j++){
+        //         System.out.print(dp[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+        // StringBuilder lcs = new StringBuilder();
+        int i=n,j=m;
+        while(i>0 && j>0){
+            if(str1.charAt(i-1) == str2.charAt(j-1)){
+                // lcs.append(str1.charAt(i-1));
+                str.append(str1.charAt(i-1));
+                i--;
+                j--;
+            }else if(dp[i-1][j]>dp[i][j-1]){
+                str.append(str1.charAt(i-1));
+                i--;
+            }
+            else{
+                str.append(str2.charAt(j-1));
+                j--;
+            }
+        }
+        while(i>0){
+            str.append(str1.charAt(i-1));
+            i--;
+        }
+        while(j>0){
+            str.append(str2.charAt(j-1));
+            j--;
+        }
+        // System.out.println(lcs.reverse().toString());
+        return str.reverse().toString();
+    }
+}
