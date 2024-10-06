ecea3342fa130050e4bfe87379f57e9e59ebd477
Pratham Srivastava <114439561+pratham534@users.noreply.github.com>
2024-10-07 00:32:44 +0530

    Time: 6 ms (14.99%), Space: 44.6 MB (88.06%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,29 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    private int[][] dp;
-    public int function(String word1, String word2, int i, int j){
-        if(j<0 && i<0) return 0;
-        if(j<0 && i>=0) return i+1;
-        if(i<0 && j>=0) return j+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(word1.charAt(i) == word2.charAt(j)){
-            return dp[i][j]=function(word1, word2, i-1, j-1);
+    private List<List<Integer>> dp;
+
+    public int function(String word1, String word2, int i, int j) {
+        if (j < 0 && i < 0)
+            return 0;
+        if (j < 0 && i >= 0)
+            return i + 1;
+        if (i < 0 && j >= 0)
+            return j + 1;
+        if (dp.get(i).get(j) != -1)
+            return dp.get(i).get(j);
+        if (word1.charAt(i) == word2.charAt(j)) {
+            int result = function(word1, word2, i - 1, j - 1);
+            dp.get(i).set(j, result);
+            return result;
         }
-        int deleteOp = 1+function(word1,word2,i-1,j);
-        int replaceOp = 1+function(word1,word2,i-1,j-1);
-        int insertOp = 1+function(word1,word2,i,j-1);
-        return dp[i][j]=Math.min(deleteOp, Math.min(replaceOp, insertOp));
+        int deleteOp = 1 + function(word1, word2, i - 1, j);
+        int replaceOp = 1 + function(word1, word2, i - 1, j - 1);
+        int insertOp = 1 + function(word1, word2, i, j - 1);
+        int result = Math.min(deleteOp, Math.min(replaceOp, insertOp));
+        dp.get(i).set(j, result);
+        return result;
     }
+
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        dp = new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                dp[i][j]=-1;
+        dp = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < m; j++) {
+                row.add(-1);
             }
+            dp.add(row);
         }
-        int ans = function(word1,word2,n-1,m-1);
-        // System.out.println(dp[n-1][m-1]);
-        return ans;
+        return function(word1, word2, n - 1, m - 1);
     }
 }
