f83d3f8c7c9d47b006e52d34c4aa936ee50e1200
Pratham Srivastava <114439561+pratham534@users.noreply.github.com>
2024-10-09 02:28:14 +0530

    Time: 2142 ms (5.13%), Space: 58.8 MB (92.47%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int ans1 = 0, ans2 = 0;
+        int n = prices.length;
+        List<Integer> dp1 = new ArrayList<>();
+        List<Integer> dp2 = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            dp1.add(0);
+            dp2.add(0);
+        }
+        dp1.set(0, 0);
+        dp2.set(n - 1, 0);
+        int mini = prices[0];
+        int maxi = 0;
+        for (int i = 1; i < n; i++) {
+            ans1 = Math.max(ans1, (prices[i] - mini));
+            dp1.set(i, ans1);
+            mini = Math.min(prices[i], mini);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            ans2 = Math.max(ans2, maxi - prices[i + 1]);
+            dp2.set(i, ans2);
+            maxi = Math.max(maxi, prices[i + 1]);
+        }
+        System.out.print("dp1: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(dp1.get(i)+",");
+        }
+        System.out.print("\ndp2: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(dp2.get(i)+",");
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, dp1.get(i) + dp2.get(i));
+        }
+        return ans;
+    }
+}
