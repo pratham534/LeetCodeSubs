185a57212235613529bf0e89a8c2bceeb0a05418
Pratham Srivastava <114439561+pratham534@users.noreply.github.com>
2024-10-09 18:34:24 +0530

    Time: 11 ms (16.64%), Space: 44.7 MB (34.73%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -4,38 +4,36 @@ import java.util.ArrayList;
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int[][] after = new int[k+1][2];
-        int[][] curr = new int[k+1][2];
-        // List<List<List<Integer>>> dp = new ArrayList<>();
-        // for(int i=0;i<=n;i++){
-        //     List<List<Integer>> dp0 = new ArrayList<>();
-        //     for(int j=0;j<=k;j++){
-        //         List<Integer> dp1 = new ArrayList<>();
-        //         for(int c=0;c<=1;c++){
-        //             dp1.add(0);
-        //         }
-        //         dp0.add(dp1);
-        //     }
-        //     dp.add(dp0);
-        // }
-            for(int j=0;j<=k;j++){
-                for(int c=0;c<=1;c++){
-                    after[j][c]=0;
-                    curr[j][c]=0;
-                }
+        // int[][] after = new int[k + 1][2];
+        // int[][] curr = new int[k + 1][2];
+        List<List<Integer>> after = new ArrayList<>();
+        List<List<Integer>> curr = new ArrayList<>();
+        for (int j = 0; j <= k; j++) {
+            List<Integer> temp = new ArrayList<>();
+            for (int c = 0; c <= 1; c++) {
+                temp.add(0);
             }
-        for(int i=n-1;i>=0;i--){
-            for(int j=1;j<=k;j++){
-                for(int c=0;c<=1;c++){
-                    if(c==1){
-                        curr[j][c]=Math.max(-prices[i]+after[j][0], 0+after[j][1]);
-                    }else{
-                        curr[j][c]=Math.max(prices[i]+after[j-1][1], 0+after[j][0]);
+            after.add(temp);
+            curr.add(temp);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 1; j <= k; j++) {
+                for (int c = 0; c <= 1; c++) {
+                    if (c == 1) {
+                        int buy = -prices[i]+after.get(j).get(0);
+                        int notBuy = 0 + after.get(j).get(1);
+                        curr.get(j).set(c, Math.max(buy, notBuy));
+                        // curr[j][c] = Math.max(-prices[i] + after[j][0], 0 + after[j][1]);
+                    } else {
+                        int sell = prices[i]+after.get(j-1).get(1);
+                        int notSell = 0 + after.get(j).get(0);
+                        curr.get(j).set(c, Math.max(sell, notSell));
+                        // curr[j][c] = Math.max(prices[i] + after[j - 1][1], 0 + after[j][0]);
                     }
                 }
             }
             after = curr;
         }
-        return after[k][1];
+        return after.get(k).get(1);
     }
 }
